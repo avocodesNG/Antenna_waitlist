659c0ef0b9167233b6bd239c2195ce3abdf73019
Dynastiy <emmanuel00michael@gmail.com>
2023-02-23 21:48:15 +0100

    added foote

---
+++ src/components/navigation/AppFooter.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="app-footer py-4 text-center">
+    <div>
+      <ul
+        class="mb-0 d-flex align-items-center justify-content-center"
+        style="gap: 20px"
+      >
+        <li class="menu-item" v-for="item in nav_menu" :key="item">
+          {{ item.title }}
+        </li>
+      </ul>
+
+      <div class="social-links">
+        <a href="javascripp:void(0)"
+          ><i class="fa fa-facebook" aria-hidden="true"></i
+        ></a>
+        <a href="javascripp:void(0)">
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+        </a>
+        <a href="javascripp:void(0)">
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+        <!-- <a href="javascripp:void(0)">
+            
+        </a> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      nav_menu: [
+        {
+          id: 1,
+          title: "FAQs",
+        },
+        {
+          id: 2,
+          title: "Privacy",
+        },
+        {
+          id: 3,
+          title: "Terms",
+        },
+        {
+          id: 4,
+          title: "Contact Us",
+        },
+        {
+          id: 5,
+          title: "Help",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -3,15 +3,17 @@
     <div>
       <AppHeader />
       <slot />
+      <AppFooter />
     </div>
   </div>
 </template>
 
 <script>
+import AppFooter from "@/components/navigation/AppFooter.vue";
 import AppHeader from "@/components/navigation/AppHeader.vue";
 
 export default {
-  components: { AppHeader },
+  components: { AppHeader, AppFooter },
   name: "HomeLayout",
   data() {
     return {};
