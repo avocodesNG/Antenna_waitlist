bfb2d7d7fab21bef227be72601c943d63bc2a823
Dynastiy <emmanuel00michael@gmail.com>
2023-03-01 10:30:53 +0100

    updated image

--- src/modules/home/components/createMerchant.vue
+++ src/modules/home/components/createMerchant.vue
@@ -26,7 +26,7 @@
           </p>
         </div>
         <div class="">
-          <img src="@/assets/img/create-merchant.png" alt="" />
+          <img src="@/assets/img/product-screens/Sign Up Screen.jpg" alt="" />
         </div>
       </div>
     </div>
