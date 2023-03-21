fd6450d78def6861f79388cda34bddb710979512
Dynastiy <emmanuel00michael@gmail.com>
2023-03-21 10:10:39 +0100

    removed vue config rules

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  rules: [
-    {
-      test: /\.(csv|xlsx|xls|zip)$/,
-      loader: "file-loader",
-      options: {
-        name: `files/[name].[ext]`,
-      },
-    },
-  ],
+  // rules: [
+  //   {
+  //     test: /\.(csv|xlsx|xls|zip)$/,
+  //     loader: "file-loader",
+  //     options: {
+  //       name: `files/[name].[ext]`,
+  //     },
+  //   },
+  // ],
 });
